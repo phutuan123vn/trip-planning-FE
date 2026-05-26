2a742d9239bed9a397d720adeada85e3da8aa8b9
Tuan Phu <phutuan123vn@gmail.com>
2026-05-26 16:09:22 +0700

    feat: add header and footer template

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const Footer: FC = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-4">
+            <p className="text-center">&copy; 2024 My Application. All rights reserved.</p>
+        </footer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+
+export const Header: FC = () => {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <h1 className="text-2xl font-bold">My Application</h1>
+        </header>
+    )
+}
